7efa6920bee5bf0c2539c391e0f8418c31a919a2
Daniel Wedding <54959410+danieldwedding@users.noreply.github.com>
2019-09-07 15:30:34 -0600

    Updated viewport

--- src/graphics/camera/Viewport.js
+++ src/graphics/camera/Viewport.js
@@ -9,13 +9,21 @@
 export class Viewport{
 
     constructor(){
+        /** left side of the view */
         this.left = 0;
+        /** right side of the view */
         this.right = 0;
+        /** the top of the view */
         this.top = 0;
+        /** the bottom of the view */
         this.bottom = 0;
+        /** width of the view */
         this.width = 0;
+        /** height of the view */
         this.height = 0;
+        /** zoom of the view */
         this.scale = {x: 0, y: 0};
+        /** frustrum of the view */
         this.viewBuffer = 100;
     }
 }
